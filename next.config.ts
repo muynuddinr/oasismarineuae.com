61bde1ee7e0f7b961bb6b92bce9492fd29c8bd69
muynuddinr <muynuddinr@gmail.com>
2025-10-17 17:37:16 +0530

    update 1

--- next.config.ts
+++ next.config.ts
@@ -28,24 +28,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.pixabay.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
         protocol: 'http',
         hostname: 'localhost',
         port: '3000',
@@ -70,14 +52,11 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       }
     ],
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    minimumCacheTTL: 60,
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   serverExternalPackages: ['mongodb'],
+  experimental: {
+    // Remove deprecated serverComponentsExternalPackages
+  }
 };
 
 export default nextConfig;
